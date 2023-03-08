e6d4b9b6edacc64ff7c3b1c6f0ea1a12ed1f12e1
jinsukims1971 <126855819+jinsukims1971@users.noreply.github.com>
2023-03-08 10:09:55 +0900

    branch test master

---
+++ src/main/java/com/gyojincompany/home/BranchTestApplication.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchTestApplication.class, args);
+	}
+
+}
